095e4c6ae14e962a2e13e10ae734174bb9f1d2f2
Luke <lukeconn@DESKTOP-1TCD6UH>
2023-12-16 18:54:54 -0600

    successfully extracted match data and placed in array

--- react-app/src/components/UserPage/userPage.js
+++ react-app/src/components/UserPage/userPage.js
@@ -1,7 +1,30 @@
 import { useSelector } from "react-redux";
+import * as riotactions from "../../store/userNames.js"
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useState } from "react";
 
 function UserPage() {
+  const dispatch = useDispatch()
   const user = useSelector((state) => state.user.userInfo);
+  const matches = useSelector((state) => state.user.matchHistory);
+  const [matchDataArr, setMatchDataArr] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = [];
+      for (let i = 0; i < matches.length; i++) {
+        console.log(matches[i])
+        let match = matches[i]
+        if(match){
+          let matchData = await dispatch(riotactions.getMatchData(match))
+          data.push(matchData)
+        }
+      }
+      setMatchDataArr(data)
+    }
+    fetchData()
+  }, [matches])
+
   if (user) {
     return (
       <div>
--- react-app/src/store/userNames.js
+++ react-app/src/store/userNames.js
@@ -1,5 +1,6 @@
 export const SET_USERDATA = "usernames/SET_USERDATA";
 export const SET_MATCHES = "usernames/SET_MATCHES";
+export const SET_MATCH_DATA = "usernames/SET_MATCH_DATA";
 
 const setUserData = (userData) => ({
     type: SET_USERDATA,
@@ -9,6 +10,11 @@ const setUserData = (userData) => ({
 const setMatches = (matches) => ({
     type: SET_MATCHES,
     matches,
+});
+
+const setMatchData = (matchData) => ({
+    type: SET_MATCH_DATA,
+    matchData,
 });
 
 
@@ -28,7 +34,7 @@ export const getUserData = (username, region) => async (dispatch) => {
 
 export const getMatches = (puuid) => async (dispatch) => {
     try {
-        const res = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-53a06b4b-e15d-45d3-ab8c-fcf8481703a2`)
+        const res = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=RGAPI-53a06b4b-e15d-45d3-ab8c-fcf8481703a2`)
         const data = await res.json()
         dispatch(setMatches(data))
         localStorage.setItem("matches", JSON.stringify(data))
@@ -39,9 +45,23 @@ export const getMatches = (puuid) => async (dispatch) => {
     }
 }
 
+export const getMatchData = (matchId) => async (dispatch) => {
+    try {
+        const res = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchId}?api_key=RGAPI-53a06b4b-e15d-45d3-ab8c-fcf8481703a2`)
+        const data = await res.json()
+        dispatch(setMatchData(data))
+        localStorage.setItem("matchData", JSON.stringify(data))
+        return data
+    }
+    catch (e) {
+        console.log("!!!!!!!!!!!!!!!!!", e, "!!!!!!!!!!!!!!!!!")
+    }
+}
+
 const initialState = {
     userInfo: JSON.parse(localStorage.getItem("userData")) || null,
-    matchHistory: JSON.parse(localStorage.getItem("matches")) || null
+    matchHistory: JSON.parse(localStorage.getItem("matches")) || null,
+    matchData: JSON.parse(localStorage.getItem("matchData")) || null
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -52,6 +72,11 @@ const usersReducer = (state = initialState, action) => {
             return { 
                 ...state,
                 matchHistory: action.matches 
+            }
+        case SET_MATCH_DATA:
+            return {
+                ...state,
+                matchData: action.matchData
             }
         default:
             return state;

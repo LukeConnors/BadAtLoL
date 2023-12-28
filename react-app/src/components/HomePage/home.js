f5fb105e04b3145b5868f7d74efeeb35eae59ef2
Luke <lukeconn@DESKTOP-1TCD6UH>
2023-12-28 12:23:40 -0600

    added styling to home page

--- react-app/src/components/HomePage/home.js
+++ react-app/src/components/HomePage/home.js
@@ -2,6 +2,7 @@ import * as riotactions from "../../store/userNames.js"
 import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { useHistory } from "react-router-dom"
+import "./home.css"
 function HomePage() {
     const dispatch = useDispatch()
     const history = useHistory()
@@ -20,39 +21,49 @@ function HomePage() {
         }
     }
 
-        return (
-            <div>
-                <h1>Home Page</h1>
-                <form onSubmit={handleSubmit}>
-                    <h2>Please enter your Summoner Name</h2>
-                    <input
-                        type="text"
-                        placeholder="Summoner Name"
-                        value={formData.username}
-                        onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-                    >
-                    </input>
-                    <h2>Please select your region</h2>
-                    <select
-                        value={formData.region}
-                        onChange={(e) => setFormData({ ...formData, region: e.target.value })}
-                    >
-                        <option value="">--Please choose an option--</option>
-                        <option value="na1">NA</option>
-                        <option value="euw1">EUW</option>
-                        <option value="eun1">EUNE</option>
-                        <option value="br1">BR</option>
-                        <option value="kr">KR</option>
-                        <option value="jp1">JP</option>
-                        <option value="la1">LAN</option>
-                        <option value="la2">LAS</option>
-                        <option value="oc1">OCE</option>
-                        <option value="tr1">TR</option>
-                        <option value="ru">RU</option>
-                    </select>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        )
+    return (
+        <div className="home-page">
+            <h1>Home Page</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-inputs">
+                    <div className="summ-name-div">
+                        <h2 className="summ-name">Summoner Name</h2>
+                        <input
+                            className="summ-name-input"
+                            type="text"
+                            placeholder="Summoner Name"
+                            value={formData.username}
+                            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                        >
+                        </input>
+                    </div>
+                    <div className="region-div">
+
+                        <h2 className="region">Region</h2>
+                        <select
+                            value={formData.region}
+                            onChange={(e) => setFormData({ ...formData, region: e.target.value })}
+                        >
+                            <option value="">--Please choose an option--</option>
+                            <option value="na1">NA</option>
+                            <option value="euw1">EUW</option>
+                            <option value="eun1">EUNE</option>
+                            <option value="br1">BR</option>
+                            <option value="kr">KR</option>
+                            <option value="jp1">JP</option>
+                            <option value="la1">LAN</option>
+                            <option value="la2">LAS</option>
+                            <option value="oc1">OCE</option>
+                            <option value="tr1">TR</option>
+                            <option value="ru">RU</option>
+                        </select>
+                    </div>
+                    <div className="submit-button-div">
+                        <button type="submit" className="submit-button">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
 }
 export default HomePage
--- react-app/src/components/UserPage/userPage.js
+++ react-app/src/components/UserPage/userPage.js
@@ -3,6 +3,7 @@ import * as riotactions from "../../store/userNames.js"
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useState } from "react";
+import "./userPage.css"
 
 function UserPage() {
   const dispatch = useDispatch()
@@ -25,11 +26,23 @@ function UserPage() {
     fetchData()
   }, [matches])
 
-  if (user) {
+  console.log("!!!!!!!!!", matchDataArr)
+  if (user && matches && matchDataArr) {
     return (
       <div>
         <h1>User Page</h1>
+        <h2>Summoner Name: {user.name}</h2>
         <h2>Summoner Level: {user.summonerLevel}</h2>
+        <img src={`https://ddragon-webp.lolmath.net/latest/img/profileicon/${user.profileIconId}.webp`}></img>
+        <h2>Match History</h2>
+        {matchDataArr.map((match) => {
+          return(
+            <div>
+              {/* <h3>Match {match.info.gameMode}</h3> */}
+            </div>
+          )
+
+        })}
       </div>
     );
   } else {

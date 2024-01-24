473098ab69ab04b32d61ab1a5f507682491d213c
Luke <lukeconn@DESKTOP-1TCD6UH>
2024-01-24 16:32:34 -0600

    added champlevel and styled

--- react-app/src/components/GameInfoModal/GameInfoModal.js
+++ react-app/src/components/GameInfoModal/GameInfoModal.js
@@ -7,7 +7,10 @@ return(
             return(
                 <div className="champion-container">
                     <h3>{participant.summonerName}</h3>
+                    <div className="img-container">
                     <img src={`https://ddragon-webp.lolmath.net/latest/img/champion/${participant.championName}.webp`}></img>
+                    <p className="champ-level">{participant.champLevel}</p>
+                    </div>
                 </div>
             )
         })}

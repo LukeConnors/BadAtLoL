9633245bba6ef11d5de7cb878bddf6bb3795f57c
Luke <lukeconn@DESKTOP-1TCD6UH>
2024-01-24 17:34:55 -0600

    added toggle in match history rather than using modal

--- react-app/src/components/HomePage/home.js
+++ react-app/src/components/HomePage/home.js
@@ -1,70 +1,75 @@
-import * as riotactions from "../../store/userNames.js"
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { useHistory } from "react-router-dom"
-import "./home.css"
+import * as riotactions from "../../store/userNames.js";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import "./home.css";
 function HomePage() {
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const [formData, setFormData] = useState({
-        username: "",
-        region: ""
-    })
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [formData, setFormData] = useState({
+    username: "",
+    region: "",
+  });
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        let user = await dispatch(riotactions.getUserData(formData.username, formData.region))
-        if (user) {
-            let matches = await dispatch(riotactions.getMatches(user.puuid))
-            history.push("/user")
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let user = await dispatch(
+      riotactions.getUserData(formData.username, formData.region)
+    );
+    if (user) {
+      let matches = await dispatch(riotactions.getMatches(user.puuid));
+      history.push("/user");
     }
+  };
 
-    return (
-        <div className="home-page">
-            <h1 className="home-page-title">Am I Bad at LoL?</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-inputs">
-                    <div className="summ-name-div">
-                        <h2 className="summ-name">Summoner Name</h2>
-                        <input
-                            className="summ-name-input"
-                            type="text"
-                            placeholder="Summoner Name"
-                            value={formData.username}
-                            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-                        >
-                        </input>
-                    </div>
-                    <div className="region-div">
-
-                        <h2 className="region">Region</h2>
-                        <select
-                            className="region-select"
-                            value={formData.region}
-                            onChange={(e) => setFormData({ ...formData, region: e.target.value })}
-                        >
-                            <option value="">--Select Region--</option>
-                            <option value="na1">NA</option>
-                            <option value="euw1">EUW</option>
-                            <option value="eun1">EUNE</option>
-                            <option value="br1">BR</option>
-                            <option value="kr">KR</option>
-                            <option value="jp1">JP</option>
-                            <option value="la1">LAN</option>
-                            <option value="la2">LAS</option>
-                            <option value="oc1">OCE</option>
-                            <option value="tr1">TR</option>
-                            <option value="ru">RU</option>
-                        </select>
-                    </div>
-                    <div className="submit-button-div">
-                        <button type="submit" className="submit-button">Submit</button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="home-page">
+      <h1 className="home-page-title">Am I Bad at LoL?</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-inputs">
+          <div className="summ-name-div">
+            <h2 className="summ-name">Summoner Name</h2>
+            <input
+              className="summ-name-input"
+              type="text"
+              placeholder="Summoner Name"
+              value={formData.username}
+              onChange={(e) =>
+                setFormData({ ...formData, username: e.target.value })
+              }
+            ></input>
+          </div>
+          <div className="region-div">
+            <h2 className="region">Region</h2>
+            <select
+              className="region-select"
+              value={formData.region}
+              onChange={(e) =>
+                setFormData({ ...formData, region: e.target.value })
+              }
+            >
+              <option value="">--Select Region--</option>
+              <option value="na1">NA</option>
+              <option value="euw1">EUW</option>
+              <option value="eun1">EUNE</option>
+              <option value="br1">BR</option>
+              <option value="kr">KR</option>
+              <option value="jp1">JP</option>
+              <option value="la1">LAN</option>
+              <option value="la2">LAS</option>
+              <option value="oc1">OCE</option>
+              <option value="tr1">TR</option>
+              <option value="ru">RU</option>
+            </select>
+          </div>
+          <div className="submit-button-div">
+            <button type="submit" className="submit-button">
+              Submit
+            </button>
+          </div>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
-export default HomePage
+export default HomePage;
--- react-app/src/components/UserPage/userPage.js
+++ react-app/src/components/UserPage/userPage.js
@@ -1,80 +1,118 @@
- import { useSelector } from "react-redux";
-import * as riotactions from "../../store/userNames.js"
+import { useSelector } from "react-redux";
+import * as riotactions from "../../store/userNames.js";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useState } from "react";
 import OpenModalButton from "../OpenModalButton";
 import GameInfoModal from "../GameInfoModal/GameInfoModal.js";
 
-import "./userPage.css"
+import "./userPage.css";
 
 function UserPage() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [selectedMatch, setSelectedMatch] = useState(null);
   const user = useSelector((state) => state.user.userInfo);
   const matches = useSelector((state) => state.user.matchHistory);
-  const [matchDataArr, setMatchDataArr] = useState([])
+  const [matchDataArr, setMatchDataArr] = useState([]);
+
+  const openMatchFunc = (matchId) => {
+    // function with callback to check if the selected match is the same one, if so it will close the match
+    // otherwise this function
+    setSelectedMatch((prevSelectedMatch) =>
+      prevSelectedMatch === matchId ? null : matchId
+    );
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const data = [];
       for (let i = 0; i < matches.length; i++) {
-        console.log(matches[i])
-        let match = matches[i]
-        if(match){
-          let matchData = await dispatch(riotactions.getMatchData(match))
-          data.push(matchData)
+        console.log(matches[i]);
+        let match = matches[i];
+        if (match) {
+          let matchData = await dispatch(riotactions.getMatchData(match));
+          data.push(matchData);
         }
       }
-      setMatchDataArr(data)
-    }
-    fetchData()
-  }, [matches])
+      setMatchDataArr(data);
+    };
+    fetchData();
+  }, [matches]);
 
-  console.log("!!!!!!!!!", matchDataArr)
+  console.log("!!!!!!!!!", matchDataArr);
   if (user && matches && matchDataArr) {
     return (
       <div>
         <h1>User Page</h1>
         <h2>Summoner Name: {user.name}</h2>
         <h2>Summoner Level: {user.summonerLevel}</h2>
-        <img src={`https://ddragon-webp.lolmath.net/latest/img/profileicon/${user.profileIconId}.webp`}></img>
+        <img
+          src={`https://ddragon-webp.lolmath.net/latest/img/profileicon/${user.profileIconId}.webp`}
+        ></img>
         <h2>Match History</h2>
-        {matchDataArr.map((match) => {
-          if(match.info.gameMode === "CLASSIC"){
-            return(
-              <div className="match-container">
-                <h3>Summoner's Rift 5v5</h3>
-                <OpenModalButton
-                  buttonText="View Match"
-                  modalComponent={<GameInfoModal match={match}/>}
-                />
-                <h4>Game Duration: {Math.floor(match.info.gameDuration / 60)} Minutes</h4>
-              </div>
-            )
-          }
+        {matchDataArr?.length > 0 ? (
+          matchDataArr?.map((match) => {
+            if (match?.info?.gameMode === "CLASSIC") {
+              return (
+                <div className="match-container">
+                  <h3>Summoner's Rift 5v5</h3>
+                  <button onClick={() => openMatchFunc(match?.info?.gameId)}>
+                    View Match
+                  </button>
+                  {selectedMatch === match.info.gameId && (
+                    <div>
+                      <h1>Match Data</h1>
+                      <div className="game-container">
+                        {match?.info?.participants?.map((participant) => {
+                          return (
+                            <div className="champion-container">
+                              <h3>{participant.summonerName}</h3>
+                              <div className="img-container" onClick={null}>
+                                <img
+                                  src={`https://ddragon-webp.lolmath.net/latest/img/champion/${participant.championName}.webp`}
+                                ></img>
+                                <p className="champ-level">
+                                  {participant.champLevel}
+                                </p>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  )}
+                  <h4>
+                    Game Duration: {Math.floor(match?.info?.gameDuration / 60)}{" "}
+                    Minutes
+                  </h4>
+                </div>
+              );
+            }
 
-          if(match.info.gameMode === "CHERRY"){
-            return(
-              <div className="match-container">
-                <h3>Arena</h3>
-                <OpenModalButton
-                  buttonText="View Match"
-                  modalComponent={<GameInfoModal match={match}/>}
-                />
-                <h4>Game Duration: {Math.floor(match.info.gameDuration / 60)} Minutes</h4>
-              </div>
-            )
-          }
-
-        })}
+            if (match.info.gameMode === "CHERRY") {
+              return (
+                <div className="match-container">
+                  <h3>Arena</h3>
+                  <OpenModalButton
+                    buttonText="View Match"
+                    modalComponent={<GameInfoModal match={match} />}
+                  />
+                  <h4>
+                    Game Duration: {Math.floor(match.info.gameDuration / 60)}{" "}
+                    Minutes
+                  </h4>
+                </div>
+              );
+            }
+          })
+        ) : (
+          <h3 className="no-matches">No Matches Found</h3>
+        )}
       </div>
     );
   } else {
-    return (
-      <>
-      </>
-    )
+    return <></>;
   }
 }
-
 
 export default UserPage;

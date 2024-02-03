df04a28b95aa9fd9a7a0fe41f45cdf12b42041a9
LukeConnors <luketconnors@yahoo.com>
2024-02-03 14:05:15 -0600

    set up function to find images of items

--- react-app/src/components/GameInfoModal/PlayerInfoModal.js
+++ react-app/src/components/GameInfoModal/PlayerInfoModal.js
@@ -1,6 +1,18 @@
 import "./playerInfoModal.css";
 
 function PlayerInfoModal({ participant }) {
+  const findImage = (item) => {
+    if (item === 0) {
+      return null;
+    } else {
+      return (
+        <img
+          src={`https://opgg-static.akamaized.net/meta/images/lol/14.2.1/item/${item}.png?image=q_auto,f_webp,w_64,h_64&v=1705466001567`}
+        ></img>
+      );
+    }
+  };
+
   return (
     <div className="main-data-container">
       <h1>{participant.riotIdGameName}</h1>
@@ -8,6 +20,13 @@ function PlayerInfoModal({ participant }) {
       <h4>
         {participant.kills}/{participant.deaths}/{participant.assists}
       </h4>
+      {findImage(participant.item0)}
+      {findImage(participant.item1)}
+      {findImage(participant.item2)}
+      {findImage(participant.item3)}
+      {findImage(participant.item4)}
+      {findImage(participant.item5)}
+      {findImage(participant.item6)}
     </div>
   );
 }

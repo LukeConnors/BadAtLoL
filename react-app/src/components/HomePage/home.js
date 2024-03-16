7851078aafc3ec50fcd2909619414a23ffa33517
LukeConnors <luketconnors@yahoo.com>
2024-03-16 01:58:38 -0500

    adding more styling

--- react-app/src/components/HomePage/home.js
+++ react-app/src/components/HomePage/home.js
@@ -50,18 +50,18 @@ function HomePage() {
                   setFormData({ ...formData, region: e.target.value })
                 }
               >
-                <option value="">--Select Region--</option>
-                <option value="na1">NA</option>
-                <option value="euw1">EUW</option>
-                <option value="eun1">EUNE</option>
-                <option value="br1">BR</option>
-                <option value="kr">KR</option>
-                <option value="jp1">JP</option>
-                <option value="la1">LAN</option>
-                <option value="la2">LAS</option>
-                <option value="oc1">OCE</option>
-                <option value="tr1">TR</option>
-                <option value="ru">RU</option>
+                <option className="region-options" value="">--Select Region--</option>
+                <option className="region-options" value="na1">NA</option>
+                <option className="region-options" value="euw1">EUW</option>
+                <option className="region-options" value="eun1">EUNE</option>
+                <option className="region-options" value="br1">BR</option>
+                <option className="region-options" value="kr">KR</option>
+                <option className="region-options" value="jp1">JP</option>
+                <option className="region-options" value="la1">LAN</option>
+                <option className="region-options" value="la2">LAS</option>
+                <option className="region-options" value="oc1">OCE</option>
+                <option className="region-options" value="tr1">TR</option>
+                <option className="region-options" value="ru">RU</option>
               </select>
             </div>
             <div className="submit-button-div">
